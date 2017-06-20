30a23856d66230f2f927f5c0cd3a1261f38af966
= <=>
2017-06-20 19:21:22 +0700

    validate dd/mm/yyyy

--- src/BaiTapJava_Phan1/BaiTapFile.java
+++ src/BaiTapJava_Phan1/BaiTapFile.java
@@ -7,7 +7,10 @@ import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class BaiTapFile {
 	public void TaoDanhSach (int n) {		
@@ -61,11 +64,87 @@ public class BaiTapFile {
 				if (HoTen.endsWith(ten)) {
 					KetQua.add(HoTen);
 				}
-			}		
+			}
+			br.close();
 		} catch (Exception e) {
 			System.out.println("Error: " + e.getMessage());
 		}
 		return KetQua;
 	}
 	
+	public void DemDauRot (BufferedReader br) {
+		int Dau = 0;
+		int Rot = 0;
+		try {
+			while (true) {
+				String st = br.readLine();
+				if (st == "" || st == null) break;
+				String[] ds = st.split("[;]");
+				if (ds[6].compareTo("Dau") == 0) {
+					Dau++;
+				} else {
+					Rot++;
+				}		
+			}
+			br.close();
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		System.out.println("So sv dau: " + Dau);
+		System.out.println("So sv rot: " + Rot);
+	}
+	
+	public Double TBC (BufferedReader br) {
+		double Tong = 0;
+		int dem = 0;
+		try{
+			while (true) {
+				String st  	= br.readLine();
+				if(st == "" || st == null) break;
+				String[] ds = st.split("[;]");
+				
+				Tong += Double.parseDouble(ds[4]);
+				dem++;			
+			}
+			br.close();
+		}catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+		return Tong/dem;
+	}
+	
+	public void validate (BufferedReader br) {
+		try {
+			while (true) {
+				String st = br.readLine();
+				if (st == "" || st == "null") break;
+				
+				String[] ds = st.split("[;]");
+				
+				String d = ds[3].replace("/", "-"); //ds[3] dang co dang dd/mm/yyyy, can chuyen qua dang dd-mm-yyyy
+				
+				SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+				Date date = format.parse(d);
+				String dcheck = "00:00:00 " + d; // chuan hoa chuoi ngay theo return cua ham toLocaleString
+				
+				if(dcheck.compareTo(date.toLocaleString()) != 0){  //toLocaleString tra ve ngay dang 00:00:00 dd-MM-yyyy
+					System.out.println(ds[0] + " : " + " dinh dang ngay sinh khong hop le! (" + ds[3] + ")");
+				}
+				
+				/*String d =  ds[3];
+				String[] date = d.split("[/]");
+				int day = Integer.parseInt(date[0]);
+				int month = Integer.parseInt(date[1]);
+				int year = Integer.parseInt(date[2]);
+			
+				if (day > 31 || month > 12 || year < 1900) {
+					System.out.println(ds[0] + " : " + " dinh dang ngay sinh khong hop le! (" + ds[3] + ")");
+				}*/
+			}
+			br.close();
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
 }
--- src/BaiTapJava_Phan1/File.java
+++ src/BaiTapJava_Phan1/File.java
@@ -8,9 +8,9 @@ import java.util.ArrayList;
 
 public class File {
 
-	public static void main(String[] args) {
-		FileInputStream fis;
+	public static void main(String[] args) {	
 		try {
+			FileInputStream fis;
 			fis = new FileInputStream("SinhVien.txt");
 			InputStreamReader isr 	= new InputStreamReader(fis);
 			BufferedReader br 		= new BufferedReader(isr);
@@ -19,13 +19,16 @@ public class File {
 			
 			//btf.TaoDanhSach(50); // chay 1 lan dau tien. lan sau thi comment no lai
 			//btf.XuatDanhSach(br);
-			ArrayList<String> HoTen = new ArrayList<String>();
+			/*ArrayList<String> HoTen = new ArrayList<String>();
 			HoTen = btf.TimKiem(br, "Phung");
 			
 			for (int i = 0; i < HoTen.size(); i++) {
 				System.out.println(HoTen.get(i));
-			}
+			}*/
 			
+			//btf.DemDauRot(br);
+			//System.out.println("Trung binh cong diem trung binh = " + btf.TBC(br));
+			btf.validate(br);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
